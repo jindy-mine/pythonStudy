4f837e308d37036decebd03822ed5bb0f2111305
jindi-mine <jindy_mine@163.com>
2020-09-30 23:59:57 +0800

    更新执行文件

---
+++ run_base.py
@@ -0,0 +1,18 @@
+"""
+=========================================
+Author:薄咊
+Time:2020/9/30  23:55
+comments:python学习
+==========================================
+"""
+from Base import stu_file1
+from Base import stu_file2
+from Base import stu_file3
+from Base import stu_file4
+from Base import stu_file5
+
+stu_file1.stu1()
+stu_file2.stu2()
+stu_file3.stu3()
+stu_file4.stu4()
+stu_file5.stu5()
