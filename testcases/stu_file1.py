941e4ff0cd2333525c3b3f488f8f51dd9c2cce01
jindi-mine <jindy_mine@163.com>
2020-10-12 16:07:35 +0800

    分支提交，更新run文件

---
+++ testcases/stu_file1.py
@@ -0,0 +1,31 @@
+"""
+=========================================
+Author:薄咊
+Time:2020/9/25  14:15
+comments:python学习-打印、输入、注释、变量命名
+==========================================
+"""
+import unittest
+
+
+class TestStu1(unittest.TestCase):
+    def test_stu1(self):
+        # 打印“hello python”
+        print("hello python")
+
+        # 这是注释，注释不执行,快捷键：ctrl+/
+        """
+        这是文档注释
+        """
+        print("第1个输出")
+        print("第2个输出")
+        print("第3个输出")
+        print("第4个输出")
+        print("第5个输出")
+
+        """
+        用户输入姓名，保存定义变量name
+        输出name
+        """
+        # name = input("输入您的姓名：")
+        # print(name)
